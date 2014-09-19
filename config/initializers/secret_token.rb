bced992ed1844fd894ecbb35afe682d957d178ab
Noah Shafi <noshaf@gmail.com>
2014-09-19 11:28:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AminLaw::Application.config.secret_token = '82d0629b7e047adf88c616ea06e5ab95c8ba577b3598d4b40a5cd88783796380e7ea88972f6a75411b1361ae818d49388957e5fa5a1f42a2d7c59c3012394e2c'
