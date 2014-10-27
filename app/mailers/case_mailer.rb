80847a8d702ed5116510a0b8dfc6e4d3aa0ddd82
shahabamin <shahabamin97@gmail.com>
2014-10-26 17:22:11 -0700

    fixed home and attorney appearance page hovering over thingy

--- app/mailers/case_mailer.rb
+++ app/mailers/case_mailer.rb
@@ -1,8 +1,12 @@
 class CaseMailer < ActionMailer::Base
   default from: "CaseEvaluations@anlegalfim.com"
 
-  def case_notification(form_info)
-    mail(:to => 'noshaf@gmail.com', :subject => "You have recieved a case evaluation")
+  def case_notification(form)
+  	@name = form[:name]
+  	@phone = form[:phone]
+  	@email = form[:email]
+  	@message = form[:message]
+    mail(:to => 'haitham.amin@hotmail.com', :subject => "You have recieved a case evaluation")
   end
 
 end
